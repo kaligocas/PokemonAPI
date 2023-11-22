b82c984db6017437de67f5a5092abc2e4f8a13b7
Nicolás Albarenque Malnero <nxcxdev@gmail.com>
2023-11-22 14:41:29 +0100

    Change view routes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/PokemonView.vue'
+import PokemonView from '../views/PokemonView.vue'
+import LoginFormView from '../views/LoginFormView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Pokemon View',
+      component: PokemonView
+    },
+    {
+      path: '/',
+      name: 'Login',
+      component: LoginFormView
     },
   ]
 })
